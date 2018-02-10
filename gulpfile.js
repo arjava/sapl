2816c6a5f09f6040eb27929968008b575330b54d
logustra <logustra@gmail.com>
2018-02-10 10:28:11 +0700

    update v1.9

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,70 @@
-var gulp = 			require("gulp"),
-	sass = 			require("gulp-sass"),
-	uglify = 		require("gulp-uglify"),
-	babel = 		require("gulp-babel"),
-	autoprefixer = 	require("gulp-autoprefixer"),
-	browserSync = 	require("browser-sync").create();
+const gulp 			= require("gulp"),
+	  sass 			= require("gulp-sass"),
+	  autoprefixer 	= require("gulp-autoprefixer"),
+	  sourcemaps 	= require('gulp-sourcemaps');
+	  browserify 	= require("gulp-browserify"),
+	  babelify 		= require("babelify"),
+	  buffer 		= require("vinyl-buffer"),
+	  uglify 		= require("gulp-uglify"),
+	  rename 		= require("gulp-rename"),
+	  image 		= require("gulp-image"),
+	  browserSync 	= require("browser-sync").create(),
+	  browsers		= "last 50 versions";
 
 // html
-gulp.task("html", function() {
+gulp.task("html", () => {
 	return gulp.src("*.html")
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream()); // livereload
+});
+
+// automatic minify images
+gulp.task("imagemin", () => {
+	gulp.src("src/images/*")
+		.pipe(image())
+		.pipe(gulp.dest("dist/images"))
+		.pipe(browserSync.stream()); // livereload
 });
 
 // compile sass
 // automatic browser prefixing
-gulp.task("sass", function() {
+gulp.task("sass", () => {
 	return gulp.src("src/sass/**/*.scss")
+		.pipe(sourcemaps.init())
 		.pipe(sass({
 			outputStyle: "compressed"
 		}).on("error", sass.logError))
-		.pipe(autoprefixer({
-			browsers : "last 20 versions"
-		}))
-		.pipe(gulp.dest("public/css"))
-		.pipe(browserSync.stream());
+		.pipe(autoprefixer({browsers}))
+		.pipe(gulp.dest("dist/css"))
+		.pipe(browserSync.stream()); // livereload
 });
 
-// babel
-// uglify
-gulp.task("babel", function () {
+// babelify
+gulp.task("babelify", () => {
 	return gulp.src("src/js/*.js")
-		.pipe(babel({
-			presets: ["es2015", "stage-2"]
+		.pipe(browserify({
+			insertGlobals: true,
+			transform: ['babelify']
 		}))
-		.pipe(uglify())		
-		.pipe(gulp.dest("public/js"))
-		.pipe(browserSync.stream());
+		.pipe(buffer())
+		.pipe(uglify()) // automatic minify javascript
+		.pipe(rename(function (path) {
+			path.basename += ".bundle";
+			path.extname = ".js"
+		}))
+		.pipe(gulp.dest("dist/js"))
+		.pipe(browserSync.stream()); // livereload
 });
 
 // watch
-// livereload
-gulp.task("watch", function() {
+gulp.task("watch", () => {
 	browserSync.init({
 		server: {
 			baseDir: "./"
 		}
 	});
-	gulp.watch("*.html", ["html"]);
+	gulp.watch("*.html", ["html", "imagemin"]);
 	gulp.watch("src/sass/**/*.scss", ["sass"]);
-	gulp.watch("src/js/*.js", ["babel"]);
+	gulp.watch("src/js/**/*.js", ["babelify"]);
 });
 
 gulp.task("default", ["watch"]);
